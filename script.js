1ad34a4e7ffb958ab2ccd608bf9f52cb92036528
Fenikend <nichiporuki2003@gmail.com>
2025-08-25 13:56:52 +0000

    Create basic structure on site.Also add some stylings.

---
+++ script.js
@@ -0,0 +1,12 @@
+const fig =['scissors','paper','rock']
+
+function getComputerChoice(){
+    let choice=Math.floor(Math.random()*3)
+    return choice
+}
+function getHumanChoice(){
+   let choice=prompt('Please choose your') 
+}
+function compareChoice(){
+
+}
