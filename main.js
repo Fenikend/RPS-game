7953f1d95b8bc95750fc88d1ca4c5024f701be6d
Fenikend <nichiporuki2003@gmail.com>
2025-09-08 14:48:22 +0300

    add function to work with buttons and simple condition check

--- main.js
+++ main.js
@@ -5,8 +5,35 @@ function setButtonStatus(){
     btn.addEventListener('click',()=>{
         persButtn.forEach((b)=>b.classList.remove('active'))
         btn.classList.add('active')
-        return(btn.id)
     })
 })
 }
 
+function getButtonStatus(){
+    persButtn.forEach(btn=>{
+        if (btn.classList.contains('active')){
+            return btn.id
+        }
+    })
+}
+
+function checkWinner(plrInput,botInput){
+    if (plrInput==='plrbtn-scissors' && botInput=='paper'){
+        return'pRound'
+    }
+    else if(plrInput==='plrbtn-paper' && botInput=='rock'){
+        return'pRound'
+    }
+    else if(plrInput==='plrbtn-rock' && botInput=='scissors')
+        return'pRound'
+    else
+        return'bRound'
+
+}
+
+function botMove(){
+    let moves={0:'scissors',1:'rock',2:'paper'}
+    console.log(moves[Math.ceil(Math.random()*3)-1])
+    
+}
+botMove()
