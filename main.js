8e4a537c2f12e87f880f7ce6a290f4e88bdab14e
Fenikend <nichiporuki2003@gmail.com>
2025-09-09 11:41:39 +0300

    Refactor getter of buttons add basic winner handler implementation

--- main.js
+++ main.js
@@ -1,39 +1,67 @@
 let persButtn=document.querySelectorAll('button')
+let persImage=document.querySelector('#user')
+let botImage=document.querySelector('#bot')
+let emptyFigure=document.querySelector('#plhFrame')
 
 function setButtonStatus(){
     persButtn.forEach((btn)=>{
     btn.addEventListener('click',()=>{
         persButtn.forEach((b)=>b.classList.remove('active'))
         btn.classList.add('active')
+        console.log(getButtonStatus())
     })
+    
 })
 }
 
 function getButtonStatus(){
-    persButtn.forEach(btn=>{
-        if (btn.classList.contains('active')){
-            return btn.id
+        for (let btn of persButtn){
+            if (btn.classList.contains('active')){
+                return btn.id.split('-')[1]
+            }
+            
         }
-    })
-}
+    }
 
-function checkWinner(plrInput,botInput){
+function setFigure(res){
+    if (res ==='pRound'){
+        persImage.classList.remove('hidden')
+    }
+    else if(res==='bRound'){
+        botImage.classList.remove('hidden')
+    }
+    else{
+        botImage.classList.contains('hidden')?'':botImage.classList.add('hidden')
+        persImage.classList.contains('hidden')?'':persImage.classList.add('hidden')
+    }
+    }
+
+function getWinner(plrInput,botInput){
     if (plrInput==='plrbtn-scissors' && botInput=='paper'){
         return'pRound'
     }
     else if(plrInput==='plrbtn-paper' && botInput=='rock'){
         return'pRound'
     }
-    else if(plrInput==='plrbtn-rock' && botInput=='scissors')
-        return'pRound'
-    else
-        return'bRound'
+    else if(plrInput==='plrbtn-rock' && botInput=='scissors'){
+        return'pRound'}
+    else{
+        return'bRound'}
 
 }
 
-function botMove(){
+function getBotMove(){
     let moves={0:'scissors',1:'rock',2:'paper'}
-    console.log(moves[Math.ceil(Math.random()*3)-1])
+    return(moves[Math.ceil(Math.random()*3)-1])
     
 }
-botMove()
+function resetRound(cnd){
+    persButtn.forEach((btn)=>{
+        if (btn.classList.contains('active')){
+            btn.classList.remove('active')
+        }
+        
+    })
+}
+setButtonStatus()
+
