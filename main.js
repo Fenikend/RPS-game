880239a55c705e2cc0086d00dfcfdcfcd27e15c8
Fenikend <nichiporuki2003@gmail.com>
2025-09-10 11:24:30 +0300

    create structure for  main game function  to further improve it

--- main.js
+++ main.js
@@ -2,13 +2,14 @@ let persButtn=document.querySelectorAll('button')
 let persImage=document.querySelector('#user')
 let botImage=document.querySelector('#bot')
 let emptyFigure=document.querySelector('#plhFrame')
+let timerElement=document.querySelector('#timer')
+let time=99
 
 function setButtonStatus(){
     persButtn.forEach((btn)=>{
     btn.addEventListener('click',()=>{
         persButtn.forEach((b)=>b.classList.remove('active'))
-        btn.classList.add('active')
-        console.log(getButtonStatus())
+        btn.classList.add('active');
     })
     
 })
@@ -21,9 +22,10 @@ function getButtonStatus(){
             }
             
         }
-    }
+    }``
 
-function setFigure(res){
+function setFigure(res=''){
+    timerElement.classList.add('hidden')
     if (res ==='pRound'){
         persImage.classList.remove('hidden')
     }
@@ -37,13 +39,16 @@ function setFigure(res){
     }
 
 function getWinner(plrInput,botInput){
-    if (plrInput==='plrbtn-scissors' && botInput=='paper'){
+    if(plrInput===botInput){
+        return 'draw'
+    }
+    else if (plrInput==='scissors' && botInput=='paper'){
         return'pRound'
     }
-    else if(plrInput==='plrbtn-paper' && botInput=='rock'){
+    else if(plrInput==='paper' && botInput=='rock'){
         return'pRound'
     }
-    else if(plrInput==='plrbtn-rock' && botInput=='scissors'){
+    else if(plrInput==='rock' && botInput=='scissors'){
         return'pRound'}
     else{
         return'bRound'}
@@ -55,13 +60,61 @@ function getBotMove(){
     return(moves[Math.ceil(Math.random()*3)-1])
     
 }
+function addScore(){
+
+}
 function resetRound(cnd){
     persButtn.forEach((btn)=>{
         if (btn.classList.contains('active')){
-            btn.classList.remove('active')
+            btn.classList.remove('active');
         }
-        
+        setFigure('')
     })
 }
-setButtonStatus()
 
+function checkTimer(){
+    let cond=getButtonStatus()
+    timerElement.textContent='00:0'+Math.floor(time/10);
+    if (cond){
+         stopTimer()
+        time=0
+    }
+    if(time===0){
+        time=0
+        stopTimer()
+    }
+}
+function stopTimer(){
+    clearInterval(myInterval)
+}
+function done(res){
+    return res
+}
+function main(){
+   setButtonStatus()
+   time=99
+   myInterval = setInterval(() => {
+        checkTimer(time)
+        time--
+
+        if (time === 0 || getButtonStatus()) {
+            let playerMove = getButtonStatus()
+            let botMove = getBotMove()
+            let res = getWinner(playerMove, botMove)
+            setFigure(res)   // you had 'res' as string
+            console.log(res)
+            clearInterval(myInterval)
+          
+        }
+    }, 100)
+    
+    
+}
+function game(){
+    
+    let round=0;
+    let results=[0,0]
+    
+    
+}
+game()
